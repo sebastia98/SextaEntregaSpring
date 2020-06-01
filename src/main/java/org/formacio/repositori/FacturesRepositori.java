93638164c9c215812179117c577c08c2d6302658
sr_Adrover <sadrover@cifpfbmoll.eu>
2020-06-01 11:08:07 +0200

    Feat: Crear metodo que devuelve facturas con nombre cliente

--- src/main/java/org/formacio/repositori/FacturesRepositori.java
+++ src/main/java/org/formacio/repositori/FacturesRepositori.java
@@ -1,5 +1,8 @@
 package org.formacio.repositori;
 
+import java.util.List;
+
+import org.formacio.domain.Client;
 import org.formacio.domain.Factura;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -12,5 +15,12 @@ public interface FacturesRepositori extends CrudRepository<Factura, Long>{
 	 */
 	@Query("SELECT sum(linfact.total) FROM Factura fact JOIN fact.linies linfact where fact.client.nom = ?1")
 	public Number totalClient(String client);
+	
+	public List<Factura> findByClientNom(String nom);
+	
+	
+	
+	
+	
 	
 }
